a3ad38b651c03cd46e4d0df2c1cc797b77eaa33f
Jesse Cooke <jesse@ratiopbc.com>
2025-08-14 17:16:44 -0700

    Add some logging to the run_test script

--- run_test.sh
+++ run_test.sh
@@ -3,8 +3,13 @@
 echo `pwd`
 cd integration_tests
 
+echo "Installing deps..."
 dbt --warn-error deps --target $1 || exit 1
+echo "Creating source table..."
 dbt --warn-error run-operation create_source_table --target $1 || exit 1
+echo "Seeding..."
 dbt --warn-error seed --target $1 --full-refresh || exit 1
+echo "Running..."
 dbt --warn-error run --target $1 || exit 1
+echo "Testing..."
 dbt --warn-error test --target $1 || exit 1
